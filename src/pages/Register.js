de59305302e12c2221846f73533ba689a1703d35
syihabudin081 <syihabupnyk@gmail.com>
2022-11-24 14:24:03 +0700

    done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -58,7 +58,7 @@ function Register() {
             src="https://cdn-icons-png.flaticon.com/512/2387/2387679.png"
             alt="logo"
           />
-          Chat App
+          Todo List
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
